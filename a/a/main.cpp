30a6dbfac470ae5d4a35b1149ab8d3b05ec21a7c
capalotmingo <55443390+capalotmingo@users.noreply.github.com>
2019-09-19 08:44:59 -0400

    Add files via upload

---
+++ a/a/main.cpp
@@ -0,0 +1,23 @@
+//Name:Laneef Mingo
+//Intro to CS
+//Period 1
+// Mr.Williams
+//2019-09-19
+
+#include <iostream>
+
+int main()
+{
+	std::cout	<< "Name:Laneef Mingo" << std::endl
+				<< "CLass Schedule" <<std::endl
+				<< "Period 1: Intro to CS" <<std::endl
+				<< "Period 3: Gym" << std::endl
+				<< "Period 4: House" << std::endl
+				<< "Period 5: Spanish" << std::endl
+				<< "Period 6: Lunch"
+				<< "Period 7: English" << std::endl
+				<< "Period 8: Geography" << std::endl
+				<< "Period 9: Geometry" << std::endl
+				<< "Period 10: ES" << std::endl;
+
+}
